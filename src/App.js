4de10efc9ec93ad3c3ebebfe6ba0aa6a2358c3fe
Forrest Jones <forrestjones2010@gmail.com>
2023-02-13 10:26:52 -0600

    Phase II Group Project 2.13.2023

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Featured from './components/Featured'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Signup from './components/Signup'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Featured />
+      <Signup />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
