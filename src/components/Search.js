4286cd844c81dd43edf400bbc2221143bfa20bc0
Forrest Jones <forrestjones2010@gmail.com>
2023-02-15 09:07:09 -0600

    added code for Search.js and added Search.css 2.15.2023

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Coin from './Coin.js';
+import './Search.css';
 
-function Search(){
-    return(
-        <div>SEARCH</div>
-    )
+function Search() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
+      .then((response) => response.json())
+      .then((data) => setCoins(data));
+  }, []);
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div className='search-container'>
+      <div className='search-bar'>
+        <input type='text' placeholder='Search Coins' onChange={handleSearch} />
+      </div>
+      <div className='coin-container'>
+        {filteredCoins.map((coin) => (
+          <Coin key={coin.id} coin={coin} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
